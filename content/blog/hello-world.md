27d7db292543885666227e6154b4b0682aac4359
Ian Mac <webguyian@gmail.com>
2013-03-24 21:41:15 -0400

    Initial commit

---
+++ content/blog/hello-world.md
@@ -0,0 +1,8 @@
+---
+title: "Hello World"
+created_at: 2013-03-24 21:37:38 -0400
+kind: article
+publish: false
+---
+
+This is my first blog post.
---
+++ lib/default.rb
@@ -0,0 +1,15 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::LinkTo
+
+module PostHelper
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end
+end
+
+include PostHelper
